94f5e61cde134b34ed62236fd5a2eecb0d285285
Giselle Brasil Macedo <giselle.macedo@cabofrio.rj.local>
2023-06-26 15:44:57 -0300

    Estrutura da aplicação - CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import { Post } from "./Post";
 
 export function App() {
   return (
-      <Post/>
+    <div>
+      <Post
+        author='Diego Fernandes'
+        content='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+      />
+      <Post
+        author='Gabriel Buzzi'
+        content='Um novo post com conteúdo'
+      />
+    </div>
   )
 }
 
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,6 +1,9 @@
-export function Post() {
+export function Post(props) {
     return (
-        <p>Post</p>
+        <div>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+        </div>
     )
 }
 
