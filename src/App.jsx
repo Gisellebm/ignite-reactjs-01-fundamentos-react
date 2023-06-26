0b18b2e4c74149cc5c6179d59aeb7c026ed8e1ad
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-06-26 20:01:53 -0300

    Estrutura da aplicação - CSS global

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post";
+import './global.css';
+import { Header } from "./components/Header";
 
 export function App() {
   return (
     <div>
+      <Header/>
       <Post
         author='Diego Fernandes'
         content='Lorem ipsum dolor sit amet consectetur adipisicing elit.'
