36ea6856d1a313a3e922b383c4d3d453acaea2cb
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-06-26 23:10:00 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 export function Sidebar() {
     return (
@@ -8,11 +10,14 @@ export function Sidebar() {
         />
 
         <div className={styles.profile}>
+            <img className={styles.avatar} src="https://github.com/Gisellebm.png" />
+
             <strong>Giselle Brasil</strong>
             <span>Web Developer</span>
         </div>
         <footer>
             <a href="#">
+                <PencilLine size={20}/>
                 Editar seu perfil
             </a>
         </footer>
