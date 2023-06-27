6afb2d75826734c476bb153565daab609c546cff
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-06-27 19:26:59 -0300

    Resolvendo problemas no git

---
+++ src/components/Post.jsx
@@ -0,0 +1,48 @@
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/Gisellebm.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Giselle Brasil</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='11 de outubro às 08:13h' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. 
+                    É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉  <a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto </a>
+                    <a href="#">#nlw </a>
+                    <a href="#">#rocketseat </a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea placeholder='Escreva um comentário...' />
+                
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment/>
+                <Comment/>
+                <Comment/>
+            </div>
+        </article>
+    )
+}
